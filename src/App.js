2ef35aabd1d73bd2a8bf228583cc8ef0f503d20b
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-04-16 21:52:58 +0630

    Enable Absolute Imports

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import {
 } from 'react-router-dom';
 
 // Components
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
-import Offers from './pages/Offers';
+import Home from 'pages/Home';
+import Profile from 'pages/Profile';
+import SignIn from 'pages/SignIn';
+import SignUp from 'pages/SignUp';
+import ForgotPassword from 'pages/ForgotPassword';
+import Offers from 'pages/Offers';
 
 function App() {
   return (
