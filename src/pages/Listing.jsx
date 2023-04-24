363c014b1fa88af112c1034711696aaa4710fb25
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-04-24 13:37:32 +0630

    Show the information in the Listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,7 +5,8 @@ import { useNavigate, useParams } from 'react-router';
 import { toast } from 'react-toastify';
 import { Autoplay, Navigation, Pagination, EffectFade } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FaShare } from 'react-icons/fa';
+import { FaBath, FaBed, FaChair, FaParking, FaShare } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
 
 // Components
 import Spinner from 'components/Spinner';
@@ -94,6 +95,85 @@ export default function Listing() {
           </span>
         )}
       </span>
+      {/* Listing content */}
+      <section className='flex flex-col md:flex-row max-w-6xl m-4 p-4 rounded-lg lg:space-x-5 shadow-lg bg-white lg:last:mx-auto'>
+        <div className='w-full'>
+          <h4 className='text-2xl font-bold mb-3 text-blue-900 '>
+            {listing.name} - $ 
+            {' '}
+            {listing.offer
+              ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            }
+            {listing.type === 'rent' && ' / Month'}
+          </h4>
+          <p className='flex items-center space-x-1 mt-6 mb-3 font-semibold'>
+            <MdLocationOn className='text-lg text-green-600' />
+            <span>{listing.address}</span>
+          </p>
+          <div className='flex justify-start items-center space-x-4 mb-5 w-[75%]'>
+            <span className='inline-block bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md'>
+              {listing.type === 'rent'? 'Rent': 'Sale'}
+            </span>
+            {listing.offer && (
+              <span className='w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md'>
+                ${+listing.regularPrice - +listing.discountedPrice} discount
+              </span>
+            )}
+          </div>
+          <p className='mb-5'>
+            <span className='font-semibold '>Description - </span>
+            {listing.description}
+          </p>          
+          <ul className='flex items-baseline space-x-3 sm:space-x-10 md:space-x-5 lg:space-x-10 mb-6 font-semibold text-xs'>
+            <li className='inline-flex items-center whitespace-nowrap space-x-1'>
+              <FaBed className='text-lg' />
+              <span>
+                {
+                  +listing.bedrooms > 1 
+                    ? `${listing.bedrooms} Beds` 
+                    : `${listing.bedrooms} Bed`
+                }
+              </span>
+            </li>
+            <li className='inline-flex items-center whitespace-nowrap space-x-1'>
+              <FaBath className='text-lg' />
+              <span>
+                {
+                  +listing.bathrooms > 1 
+                    ? `${listing.bathrooms} Baths` 
+                    : `${listing.bathrooms} Bath`
+                }
+              </span>
+            </li>
+            <li className='inline-flex items-center whitespace-nowrap space-x-1'>
+              <FaParking className='text-lg' />
+              <span>
+                {
+                  listing.parking 
+                    ? 'Parking spot' 
+                    : 'No parking'
+                }
+              </span>
+            </li>
+            <li className='inline-flex items-center whitespace-nowrap space-x-1'>
+              <FaChair className='text-lg' />
+              <span>
+                {
+                  listing.furnished 
+                    ? 'Furnished' 
+                    : 'Not furnished'
+                }
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className='bg-blue-300 w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden'></div>
+      </section>
     </main>
   );
 }
