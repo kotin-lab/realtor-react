0d083bc3ed6612c13a56c0b99dd8665775716ac1
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-04-28 15:21:07 +0630

    Complete the Offers page

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import spinner from 'assets/svg/spinner.svg'
 
 export default function Spinner() {
   return (
-    <div className='bg-black bg-opacity-50 flex items-center justify-center fiexd inset-0 z-40 w-screen h-screen'>
+    <div className='bg-black bg-opacity-50 flex items-center justify-center fixed inset-0 z-40 w-screen h-screen'>
       <div>
         <img src={spinner} alt='Loading...' className='h-24' />
       </div>
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,125 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from 'firebase.config';
+import { 
+  collection, 
+  getDocs, 
+  limit, 
+  orderBy, 
+  query, 
+  startAfter, 
+  where 
+} from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+// Components
+import Spinner from 'components/Spinner';
+import ListingItem from 'components/ListingItem';
 
 export default function Offers() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+  const queryLimit = 4;
+
+  /** Hooks */
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingRef = collection(db, 'listings');
+        const q = query(
+          listingRef, 
+          where('offer', '==', true), 
+          orderBy('timestamp', 'desc'),
+          limit(queryLimit)
+        );
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+        querySnap.forEach(doc => {
+          listings.push({
+            id: doc.id,
+            data: doc.data()
+          });
+        });
+        setListings(listings);
+        setLoading(false);
+        
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+        toast.error('Could not fetch listings');
+      }
+    }
+    fetchListings();
+  }, []);
+
+  // Handlers
+  async function handleFetchMoreListings() {
+    try {
+      const listingRef = collection(db, 'listings');
+      const q = query(
+        listingRef, 
+        where('offer', '==', true), 
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(queryLimit)
+      );
+      const querySnap = await getDocs(q);
+
+      const listings = [];
+      querySnap.forEach(doc => {
+        listings.push({
+          id: doc.id,
+          data: doc.data()
+        });
+      });
+      setListings(prevState => ([
+        ...prevState,
+        ...listings
+      ]));
+      setLoading(false);
+      
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      toast.error('Could not fetch listings');
+    }
+  }
+
   return (
-    <div>Offers</div>
+    <section className='max-w-6xl mx-auto px-3'>
+      <h1 className='text-3xl text-center font-bold mt-6 mb-4'>Offers</h1>
+      {loading
+        ? <Spinner />
+        : listings && listings.length > 0 
+        ? (
+          <>
+            <main>
+              <ul className='sm:grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+                {listings.map(({id, data}) => (
+                  <ListingItem key={id} id={id} listing={data} />
+                ))}
+              </ul>
+            </main>
+            {lastFetchedListing && (
+              <div className='flex justify-center items-center'>
+                <button 
+                  onClick={handleFetchMoreListings}
+                  className='bg-white px-3 py-1.5 text-gray-700 border border-gray-300 hover:border-slate-600 rounded transition duration-150 ease-in-out my-6'
+                >
+                    Load more
+                </button>
+              </div>
+            )}
+          </>
+        )
+        : (
+          <p>There are no current offers</p>
+        )}
+    </section>
   )
 }
